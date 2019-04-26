cc6b3ea37ce47e372120947c27215709332d22c9
Lucas <lucas.sanchez@epitech.eu>
2019-04-26 11:57:04 +0200

    feat(master) API now get arguments for MakefileRulesExec

--- server/index.js
+++ server/index.js
@@ -7,11 +7,23 @@ const root = '/api/v1';
 app.get(root + '/getMakefileRulesExecution', (req, res) => {
     console.log(req.ip + ' is connecting to getMakefileRulesExecution');
 
-    const output = shell.exec("./../../Script/verif_repo.sh simon1.provost PSU_minishell1_2017 mysh master");
-    //todo /*we need to get the login / name project / bynary name plus branch choice */
+    const userName = req.query.userName;
+    const projectName = req.query.projectName;
+    const binaryName = req.query.binaryName;
 
-    res.json({
-        output: output.stdout,npm install pm2 -g
+    if (userName === undefined || projectName === undefined || binaryName === undefined)
+        return res.json({
+            output: "Missing Argument",
+            code: 1,
+        });
+
+
+    console.log("./Scripts/verif_repo.sh " + userName + " " + projectName + " " + binaryName + " master");
+
+    const output = shell.exec("./Scripts/verif_repo.sh " + userName + " " + projectName + " " + binaryName + " master");
+
+    return res.json({
+        output: output.stdout,
         code: output.code,
     });
 });
