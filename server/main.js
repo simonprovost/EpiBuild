d384e6f030758a24c3bb811a268cb8bb0c7439b5
SimonProvost <simon1.provost@epitech.eu>
2019-04-30 19:15:07 +0200

    Fix(master): Fix backslash n (one new) in the label stdout/stderr in the server.

--- server/main.js
+++ server/main.js
@@ -21,7 +21,7 @@ app.get(root + '/getMakefileRulesExecution', (req, res) => {
     const output = shell.exec("./Scripts/verif_repo.sh " + userName + " " + projectName + " " + binaryName + " " + branchName);
 
     return res.json({
-        output: "##### Stdout: \n" + output.stdout + "##### Stderr: \n" + output.stderr,
+        output: "##### Stdout: \n\n" + output.stdout + "##### Stderr: \n\n" + output.stderr,
         code: output.code,
     });
 });
